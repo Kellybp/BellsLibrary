c435d309b9a1f5082d290d1166616647940afe30
Kellybp <kellybp11235@outlook.com>
2024-05-10 13:46:12 -0400

    Development of Initial Classes and Database

---
+++ Models/Loan.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BellsLibrary.Models
+{
+    public class Loan
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public required int BookId { get; set; }
+        
+        [ForeignKey("BookId")]
+        public required Book Book { get; set; }
+
+        [Required]
+        public required int AccountId { get; set; }
+
+        [ForeignKey("AccountId")]
+        public required Account Account { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public required DateTime LoanDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ReturnedDate { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
+using BellsLibrary.Data;
+using BellsLibrary.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<DataSeeder>();
+
+builder.Services.AddDbContext<LibraryContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -15,11 +28,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
+//app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
